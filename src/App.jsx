7576141f5a785a6a3ff2280bc89ef94f5fbe1c1a
jayanthsrinivasan <jayanth.srinivasan@silencespeaks.io>
2025-08-03 23:33:03 +0530

    Added TailwindCSS and DaisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
+import "tailwindcss"
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <NavBar/>
+      <h1 class="text-3xl font-bold underline">Hello</h1>
     </>
   )
 }
